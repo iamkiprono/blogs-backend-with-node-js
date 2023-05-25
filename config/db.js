29160909eeae1b78287efd36c16b63cc5e4c7e48
Collins Kiprono Kirui <101986591+iamkiprono@users.noreply.github.com>
2023-05-25 11:44:00 +0000

    Update config folder

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDb = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://collinskirui23:colo123@cluster0.hnhuyzz.mongodb.net/')
+        const conn = await mongoose.connect('mongodb+srv://collinskirui23:colo123@cluster0.hnhuyzz.mongodb.net/blogsdb')
         console.log(`Connected ${conn.connection.host}`)
     } catch (error) {
         console.log(error)
